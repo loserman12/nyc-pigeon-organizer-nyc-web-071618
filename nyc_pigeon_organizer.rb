b31ac72b779c95b00248e194e6048def283f5531
Your Name <you@example.com>
2018-07-08 18:48:23 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+    data.each do |attributes_hash, values_hash|
+      values_hash.each do |array_key, array_value|
+        array_value.each do |name|
+          if !pigeon_list.has_key?(name)
+#if theres not a key w/ the varible name  
+            pigeon_list[name] = {
+              :color => [],
+              :gender => [],
+              :lives => []
+            }
+          end
+          pigeon_list[name][attributes_hash] << array_key.to_s
+        end
+      end
+    end
+    pigeon_list
 end
